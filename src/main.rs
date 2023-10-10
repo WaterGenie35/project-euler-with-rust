9293411fdb4b828f3125f2c7ff18d5c6f3a80a83
WaterGenie35 <thirdwatergenie@gmail.com>
2023-10-10 23:09:39 +0700

    Add solution to problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    solution1();
+}
+
+fn solution1() {
+    // Find sum of multiples of 3 or 5 below 1000
+    let mut sum = 0;
+    let mut counter = 1;
+    while counter < 1000 {
+        if  counter % 3 == 0 || counter % 5 == 0 {
+            sum += counter;
+        }
+        counter += 1;
+    }
+    println!("Sum of multiples of 3 or 5 below 1000: {sum}");
 }
