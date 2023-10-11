5e7aeabedfc614455d89285cd8f0758a69662808
WaterGenie35 <thirdwatergenie@gmail.com>
2023-10-11 23:37:18 +0700

    Add thousand separators

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ fn main() {
 }
 
 fn solution1() {
-    // Find sum of multiples of 3 or 5 below 1000
+    // Find sum of multiples of 3 or 5 below 1,000
     let mut sum = 0;
     let mut counter = 1;
-    while counter < 1000 {
+    while counter < 1_000 {
         if  counter % 3 == 0 || counter % 5 == 0 {
             sum += counter;
         }
         counter += 1;
     }
-    println!("Sum of multiples of 3 or 5 below 1000: {sum}");
+    println!("Sum of multiples of 3 or 5 below 1,000: {sum}");
+}
 }
