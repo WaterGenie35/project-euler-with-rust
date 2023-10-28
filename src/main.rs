0f4233e25d3258eb87e5548be4bbc62e47e39d9a
WaterGenie35 <thirdwatergenie@gmail.com>
2023-10-29 02:01:31 +0700

    Add solution to problem 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
+    // TODO move to separate files, cli specifies which solution to run
     solution1();
+    solution2();
 }
 
 fn solution1() {
@@ -14,4 +16,20 @@ fn solution1() {
     }
     println!("Sum of multiples of 3 or 5 below 1,000: {sum}");
 }
+
+fn solution2() {
+    // Find sum of all even fibonacci numbers <= 4,000,000
+    // (Doesn't matter for this particular exercise, but starts the sequence with 1 and 2)
+    // See notes:
+    // https://github.com/WaterGenie35/project-euler-with-rust/wiki/Problem-2-%E2%80%90-Even-Fibonacci-Numbers
+    let mut sum = 2;
+    let mut prev = 2;
+    let mut head = 8;
+    while head <= 4_000_000 {
+        sum += head;
+        let tmp = head;
+        head = (4 * head) + prev;
+        prev = tmp;
+    }
+    println!("Sum of even fibonacci <= 4,000,000: {sum}");
 }
